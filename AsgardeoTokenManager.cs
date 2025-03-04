bbf2b06992a886f752636ad2426110ee6427aeb3
Nethiniiii <nethipererassp@gmail.com>
2025-03-04 11:33:44 +0530

    Add files via upload

---
+++ AsgardeoTokenManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class AsgardeoTokenManager : MonoBehaviour
+{
+    private string tokenUrl = "https://api.asgardeo.io/t/YOUR_TENANT/oauth2/token";
+    private string clientId = "YOUR_CLIENT_ID";
+    private string redirectUri = "com.fitton://oauth2redirect";
+    private string codeVerifier;
+    
+    public IEnumerator GetAccessToken(string authCode, System.Action<string> onSuccess)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("grant_type", "authorization_code");
+        form.AddField("client_id", clientId);
+        form.AddField("redirect_uri", redirectUri);
+        form.AddField("code", authCode);
+        form.AddField("code_verifier", codeVerifier);
+
+        using (UnityWebRequest request = UnityWebRequest.Post(tokenUrl, form))
+        {
+            request.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                Debug.Log("Access Token Response: " + request.downloadHandler.text);
+                onSuccess(request.downloadHandler.text);
+            }
+            else
+            {
+                Debug.LogError("Error Getting Token: " + request.error);
+            }
+        }
+    }
+}
+
