6c396bae77e1915a9f2f5ebb6a7dedd2b3932188
Seniru Pathirana <seniru108@gmail.com>
2025-03-07 17:48:41 +0530

    Minor Changes

--- api/TestServer.py
+++ api/TestServer.py
@@ -87,7 +87,4 @@ def download_model():
 
 
 
-# Run the server
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Railway assigns a dynamic port
-    uvicorn.run(app, host="0.0.0.0", port=port)
+
