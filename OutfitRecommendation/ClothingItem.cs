18c2c4ec23200afd46f947c7251ba04833e9237a
vishnu2004-bit <vishnu.20230001@iit.ac.lk>
2025-03-23 15:48:53 +0530

    Added Comments

--- OutfitRecommendation/ClothingItem.cs
+++ OutfitRecommendation/ClothingItem.cs
@@ -1,6 +1,6 @@
 using System;
 
-[Serializable]
+[Serializable] // Allows this class to be serialized (e.g., saved to a file or sent over a network)
 public class ClothingItem
 {
     public string Name;
@@ -9,6 +9,7 @@ public class ClothingItem
     public string Season; // e.g., "Summer", "Winter", "All"
     public string Style; // e.g., "Casual", "Formal", "Sporty"
 
+//Constructor to initialise all properties when creating a new item
     public ClothingItem(string name, string type, string color, string season, string style)
     {
         Name = name;
