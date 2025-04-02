c4cb91b2fbef16e18efa8560b054e48f0eafe9a1
vishnu2004-bit <vishnu.20230001@iit.ac.lk>
2025-04-02 23:11:16 +0530

    added support to visually display each clothing item's image using a prefab and container in the UI.
    I added functionality to visually display each recommended clothing item by introducing an image container and a prefab in the UI. This allows the app to dynamically create and show image representations (Sprite) of the clothing items alongside the text description. The imageContainer holds all the item images, and imagePrefab serves as a template (with a UI Image component) that gets cloned and updated with each item's sprite when an outfit is displayed. This makes the recommendations more engaging and user-friendly by combining visuals with text.

--- OutfitRecommendation/UIManager.cs
+++ OutfitRecommendation/UIManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public Text recommendationText;
+    public Transform imageContainer; // 🔸 New: Container to hold clothing item images
+    public GameObject imagePrefab;   // 🔸 New: Prefab with an Image component
     public ClothingRecommender recommender;
 
     void Start()
@@ -22,10 +24,24 @@ public class UIManager : MonoBehaviour
     void ShowOutfit(List<ClothingItem> outfit)
     {
         recommendationText.text = "Recommended Outfit:\n";
+
+        // 🔸 Clear old images
+        foreach (Transform child in imageContainer)
+        {
+            Destroy(child.gameObject);
+        }
+
         foreach (var item in outfit)
         {
-            // 🔸 Modified this line to include color info
             recommendationText.text += $"{item.Name} ({item.Type}) - {item.Color}\n";
+
+            // 🔸 Instantiate image prefab and assign sprite
+            if (item.Sprite != null)
+            {
+                GameObject imageObj = Instantiate(imagePrefab, imageContainer);
+                Image img = imageObj.GetComponent<Image>();
+                img.sprite = item.Sprite;
+            }
         }
     }
 }
